b59f2c8ca2461cf3586498f786bf7b09b7abde07
Dayorboye <116425062+Dayorboye@users.noreply.github.com>
2023-04-16 18:14:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import pathlib
 import pandas as pd
 import dash
 import dash_html_components as html
@@ -7,16 +6,9 @@ from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
 import plotly.express as px
 from dash import no_update
-# from jobs import app
-# from jobs.routes import *
 
-import sys
-print(sys.version)
 # Dash Application
 
-# PATH = pathlib.Path(__file__).parent
-# DATA_PATH = PATH.joinpath("data").resolve()
-# datasets = pd.read_csv(DATA_PATH.joinpath("lagosHosptals.xlsx"))
 
 app = dash.Dash(__name__)
 
@@ -41,49 +33,16 @@ geo_datas["lon"]
 
 
 K_clusters = range(1,10)
-kmeans = [KMeans(n_clusters=i) for i in K_clusters]
-Y_axis = geo_datas[['lon']]
-X_axis = geo_datas[['lat']]
-score = [kmeans[i].fit(Y_axis).score(Y_axis) for i in range(len(kmeans))]
-# Visualize
+
+score = pd.read_csv('scoree.csv')
+score = score.stack().tolist()
 line_fig = px.line(x = K_clusters, y = score, title = 'Elbow Curve')
 
 
-KMNs = KMeans(n_clusters=6)
-
-KMNs.fit(geo_datas)
-
-Labels = KMNs.predict(geo_datas)
-
-Centroid = KMNs.cluster_centers_
-a = Centroid[:,0]
-b = Centroid[:,1]
-
-from geopandas.tools import geocode
-import pandas as pd
-from geopy.geocoders import ArcGIS
-
-geolocator = ArcGIS()
-
-Centriod_df = pd.DataFrame(Centroid, columns=["Latitude","Longitude"])
-Centriod_df['geo_coord'] = Centriod_df.apply(lambda row: (str(row.Latitude),str(row.Longitude)),axis=1)
-Centriod_df['address'] = Centriod_df.apply(lambda row: geolocator.reverse(row.geo_coord).address,axis=1)
-
-Centroid = Centriod_df[['Latitude','Longitude','address']]
-Centroid
-
+Labels = pd.read_csv('Labels.csv')
+Labels = Labels.to_numpy().flatten()
 fig = px.scatter(geo_datas, x = geo_datas["lat"], y = geo_datas["lon"],
               color = Labels, opacity = 0.8, size = geo_datas["lat"], size_max=30, title = 'LAGOS HOSPITAL CLUSTER')
-
-
-fig_table = go.Figure(data=[go.Table(
-    header=dict(values=list(Centroid.columns),
-                fill_color='paleturquoise',
-                align='left'),
-    cells=dict(values=[Centroid.Latitude, Centroid.Longitude, Centroid.address],
-               fill_color='lavender',
-               align='left'))
-])
 
 
 # Application layout
@@ -95,8 +54,8 @@ app.layout = html.Div(children=[
                         html.Div(children=[html.Iframe(id = 'map', srcDoc = open('Hospital_around_Lagos.html', 'r').read(), width ='100%', height = '600',)],
                         style={"background-color": "#0e1012","padding": "65px","justify-content": "space-around"}),
                         
-                        html.Div(children=[dcc.Graph(id='plot', figure= fig_table)],
-                        style={"background-color": "#0e1012","padding": "65px", "justify-content": "space-around","height":"50vh"}),
+#                         html.Div(children=[dcc.Graph(id='plot', figure= fig_table)],
+#                         style={"background-color": "#0e1012","padding": "65px", "justify-content": "space-around","height":"50vh"}),
                         
                         html.Div(children=[html.Iframe(id = 'map1', srcDoc = open('Centroid_Hosp.html', 'r').read(), width ='100%', height = '600',)],
                         style={"background-color": "#0e1012","padding": "65px","justify-content": "space-around"}),
@@ -126,7 +85,3 @@ app.layout = html.Div(children=[
 # Run the app
 if __name__ == '__main__':
       app.run_server()
-
-
-
-
