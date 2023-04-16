8fbb79350a24a8dd205d950bc0f0cc812824a5a8
Dayorboye <116425062+Dayorboye@users.noreply.github.com>
2023-04-16 19:07:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import dash_core_components as dcc
 from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
 import plotly.express as px
-import openpyxl
 
 
 # Dash Application
