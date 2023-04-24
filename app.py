474abd8b77bca0387bd49627075661bdb7724162
Dayorboye <116425062+Dayorboye@users.noreply.github.com>
2023-04-24 23:47:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import dash_bootstrap_components as dbc
 
 
 # app = dash.Dash(__name__)
-app = Dash(external_stylesheets=[dbc.themes.SLATE])
+app = dash.Dash(__name__,  external_stylesheets=[dbc.themes.SLATE])
 
 server = app.server
 
